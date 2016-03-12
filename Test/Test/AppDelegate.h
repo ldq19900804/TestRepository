77fbc932a012fa24d0595462e3827210bb94c0fb
ldq19900804 <mac@MacdeiMac-87.local>
2016-03-12 14:55:17 +0800

    ldq
    add new  project

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 ldq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 ldq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
